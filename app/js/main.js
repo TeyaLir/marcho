4d86851e5cda87b10ad984fdc6cf2d38c81cad47
TeyaLir <ia_88@bk.ru>
2021-09-28 20:57:30 +0300

    Add product section

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,19 @@
 $(function(){
   
+  // Слайдер на главной странице
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true,
     autoplaySpeed: 3000
+  });
+
+  // Звёздный рейтинг
+  $(".star").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
   });
 });
